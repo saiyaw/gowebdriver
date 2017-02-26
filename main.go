f8a2c173cfe919c5f2d14338d7402d77f2bbacf6
Ruby Wang <Ruby@RubyMBP.lan>
2017-02-27 01:34:34 +0800

    first example.

---
+++ main.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"github.com/sclevine/agouti"
+	"log"
+	"time"
+)
+
+func main() {
+	driver := agouti.ChromeDriver()
+	if err := driver.Start(); err != nil {
+		log.Fatalf("Failed to start driver : %v", err)
+	}
+	defer driver.Stop()
+
+	page, err := driver.NewPage(agouti.Browser("chrome"))
+	if err != nil {
+		log.Fatalf("failed to open page : %v", err)
+	}
+
+	err = page.Navigate("http://www.jd.com")
+	if err != nil {
+		log.Fatalf("failed to navigate the site : %v", err)
+	}
+
+	loginLink := page.FindByClass("link-login")
+	log.Println(loginLink)
+	err = loginLink.Click()
+	if err != nil {
+		log.Fatalf("failed to click the login link : %v", err)
+	}
+
+	time.Sleep(time.Second * 5)
+
+	loginTab := page.Find(".login-tab.login-tab-r")
+	log.Println(loginTab)
+	err = loginTab.Click()
+	if err != nil {
+		log.Fatalf("failed to click the login tab ： %v", err)
+	}
+
+	time.Sleep(time.Second * 5)
+
+	userNameEntry := page.FindByID("loginname")
+	userNameEntry.SendKeys("gray_king@163.com")
+
+	userPassEntry := page.FindByID("nloginpwd")
+	userPassEntry.SendKeys("wang123456")
+
+	loginSubmit := page.FindByID("loginsubmit")
+	loginSubmit.Click()
+
+	time.Sleep(time.Second * 5)
+
+	searchTextInput := page.FindByID("key")
+	log.Println(searchTextInput)
+	err = searchTextInput.SendKeys("数据线")
+	if err != nil {
+		log.Fatalf("failed to input the  search text.  ： %v", err)
+	}
+
+	searchButton := page.FindByClass("button")
+	log.Println(searchButton)
+	err = searchButton.Click()
+	if err != nil {
+		log.Fatalf("failed to click the search button ： %v", err)
+	}
+
+	time.Sleep(time.Second * 10)
+
+	page.Refresh()
+
+	time.Sleep(time.Second * 10)
+
+}
